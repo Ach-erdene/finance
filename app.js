349dcd338792fbb1ccccc3012a73f5fef2862e98
Ach-erdene <aachka8@gmail.com>
2023-09-14 20:50:34 +0800

    Finance module hiij ehelsen

--- app.js
+++ app.js
@@ -2,17 +2,62 @@
 //Delgetstei ajillah controller
 var uiController = (function(){
 
+      var DOMStrings = {
+            inputType : '.add__type',
+            inpuDesc : '.add__description',
+            inputValue : '.add__value',
+            addBtn : '.add__btn'
+
+
+      };
+      
+      return{
+            getInput : function(){
+                  return {
+                        type : document.querySelector(DOMStrings.inputType).value,
+                        desc : document.querySelector(DOMStrings.inpuDesc).value,
+                        value : document.querySelector(DOMStrings.inputValue).value,
+                  };
+            },getDOMStrings : function(){
+                  return DOMStrings
+            }
+
+      };
+
 })();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Sanhuutei ajillah controller
 var FinanceController = (function(){
+      var Income = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+      }    
+      var Expense = function(id, description, value) {
+            this.id = id;
+            this.description = description;
+            this.value = value;
+      }
+      var data = {
+            allItems : {
+                  inc : [],
+                  exp : []
+            },
+            totals : {
+                  inc : 0,
+                  exp : 0,
+            }
+      }
+
 
 })();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Programmiin holbogch controller
 var AppController = (function(uiController, FinanceController){
+
+      
       
       var ctrlAddItem = function(){
             
@@ -27,15 +72,28 @@ var AppController = (function(uiController, FinanceController){
 
             //5 etsesiin uldegdel tootsoog delgetsend gargana
       }
-      document.querySelector('.add__btn').addEventListener('click', function(){
+      
+      var setUpEventListeners = function() {
+            var DOM = uiController.getDOMStrings();
+            document.querySelector(DOM.addBtn).addEventListener('click', function(){
 
-            ctrlAddItem();
-      })
-      document.addEventListener('keypress', function(event){
-            if(enter.keycode === 13 || event.which === 13){
                   ctrlAddItem();
+            })
+            document.addEventListener('keypress', function(event){
+                  if(enter.keycode === 13 || event.which === 13){
+                        ctrlAddItem();
+      
+                  }
+            })
 
+      };
+
+
+      return{
+            init : function(){
+                  console.log('app ehellee');
+                  setUpEventListeners();
+                  
             }
-
-      })
+      }
 })(uiController, FinanceController);
